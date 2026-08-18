69028f9e2faab5d481bb8d5b1a119399f821903f
Sebastian Penalver <sa.penalver@gmail.com>
2026-08-18 14:01:04 +0100

    add latest updates

--- index.js
+++ index.js
@@ -3,21 +3,16 @@ import axios from "axios";
 
 const app = express();
 const port = 4000;
+const API_URL = "https://api.hyrule-compendium.com/v3/regions"
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", async (req, res) => {
-
-    const result = await axios.get("https://api.hyrule-compendium.com/v3/regions/eldin");
-    console.log(result.data);
-    res.render("index.ejs")
+    res.render("index.ejs", {data: "select a region"})
 })
 
 
 app.listen(port, ()=> {
     console.log(`Eureka on port ${port}`);
 })
-
-/* Console log an api value
-   Git + version control :) */
