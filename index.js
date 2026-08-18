3dd7e33ded20b278d7a1b0d637a322ff3d41e727
Sebastian Penalver <sa.penalver@gmail.com>
2026-08-18 15:03:12 +0100

    add loop to navbar

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ import axios from "axios";
 
 const app = express();
 const port = 4000;
-const API_URL = "https://api.hyrule-compendium.com/v3/regions"
+const API_URL = "https://api.hyrule-compendium.com/v3/regions/all"
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", async (req, res) => {
-    res.render("index.ejs", {data: "select a region"})
+    const result = await axios.get(API_URL);
+
+    res.render("index.ejs", {
+        data: "select a region",
+        regions: result.data.data
+    })
 })
 
 
 app.listen(port, ()=> {
     console.log(`Eureka on port ${port}`);
 })
+
+
+/* Make a nav bar using thie https://www.youtube.com/watch?v=R7b3OlEyqug */
