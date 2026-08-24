db1b8121fee513827afcf0999c1aedb3fe8b57eb
Sebastian Penalver <sa.penalver@gmail.com>
2026-08-24 10:17:46 +0100

    add shrines from API

--- index.js
+++ index.js
@@ -4,17 +4,30 @@ import axios from "axios";
 const app = express();
 const port = 4000;
 const API_URL = "https://api.hyrule-compendium.com/v3/regions/all"
+const API_REG = "https://api.hyrule-compendium.com/v3/regions/"
 
 app.use(express.static("public"));
 app.use(express.urlencoded({extended: true}));
 
 app.get("/", async (req, res) => {
-    const result = await axios.get(API_URL);
-
+    const options = await axios.get(API_URL);
     res.render("index.ejs", {
-        data: "select a region",
-        regions: result.data.data
+        regions: options.data.data
     })
+})
+
+
+app.get("/region/:name",async (req, res) => {
+    const region = req.params.name;
+    const result = await axios.get(`${API_REG}/${region}`)
+    const shrines = result.data.data.shrines;
+    const options = await axios.get(API_URL);
+    const regionTitle= region.toUpperCase();
+    res.render("index.ejs", {
+        region: regionTitle,
+        shrines:shrines,
+        regions: options.data.data
+    });
 })
 
 
